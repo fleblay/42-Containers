f9bfda049c99aaf35763747b4912986a064a7bba
Frederic Le Blay <frederic.leblay@gmail.com>
2023-01-05 19:15:59 +0100

    wip testers

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -80,9 +80,10 @@ namespace ft
 		void			pop_back(void);
 		iterator		insert(iterator position, const value_type &val);
 		void			insert(iterator position, size_type n, const value_type &val);
-		template <class InputIterator>
+//		template <class InputIterator>
 //		void			insert(iterator position, typename enable_if<!is_integral<InputIterator>::value, InputIterator>::type first,
 //							InputIterator last);
+		template <class InputIterator>
 		void			insert(iterator position, InputIterator first, InputIterator last, typename enable_if<!is_integral<InputIterator>::value, InputIterator>::type* = 0);
 		iterator		erase(iterator position);
 		iterator		erase(iterator first, iterator last);
